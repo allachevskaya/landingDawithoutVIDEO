3e67266a9e16958d760fc9eec612499982aaa85f
allachevskaya <alprel@list.ru>
2023-03-09 18:55:41 +0300

    changes

--- scripts/sliderMainPage.js
+++ scripts/sliderMainPage.js
@@ -1,45 +1,14 @@
-const favoriteIcon = document.querySelectorAll('#loveWhite');
-const arrayFavoriteIcon = ['assets/icons/loveWhite.svg', 'assets/icons/loveWhiteFull.svg']
-const slides = document.querySelectorAll('.NewProductsSliderSlides');
-const slidesMob = document.querySelectorAll('.NewProductsMobSliderItem')
-const mainImg = document.querySelector('#MainImg');
+const favoriteIcon = document.querySelectorAll('#addFavorite');
+const favoriteIconMob = document.querySelectorAll('#addFavorite-mob')
+const arrayFavoriteIcon = ['assets/icons/loveWhite.svg', 'assets/icons/loveWhiteFull.svg'];
 let i = 0;
-let state = 0;
-let stateMob = 0;
 
-handleSrc();
-
-function handleSrc() {
-
-    favoriteIcon.forEach(icon => {
-        icon.addEventListener('click', (e) => {
-            i++;
-            if (i == arrayFavoriteIcon.length) {
-                i = 0;
-            }
-            e.target.src = arrayFavoriteIcon[i];
-        });
-    });
-
-    // window.addEventListener("resize", () => {
-    //     if (window.innerWidth < 680) {
-    //         mainImg.src = '../assets/mainPage/mainIMg.png';
-           
-    //     }
-    //     else {
-    //         mainImg.src = '../assets/mainPage/IMG_6884.jpg';
-           
-    //     }
-    // });
-
-}
+const slidesDesk = document.querySelectorAll('.NewProductsSlider');
+const slidesMob = document.querySelectorAll('.SliderItemMob');
 
 
-getSlider(slides) // слайдер
-getSlider(slidesMob) // слайдер
+function getSlider(array, state) {
 
-function getSlider(array) {
-    
     const activeSlide = index => {
 
         for (let slide of array) {
@@ -62,7 +31,45 @@ function getSlider(array) {
 
 
 
-    let timerId = setInterval(() => nextSlide(), 4000);
+    let timerId = setInterval(() => nextSlide(), 4900);
 }
+
+
+
+
+function changeFavoriteIcon (icons) {
+    i = 0;
+    icons.forEach(icon => {
+        
+        icon.addEventListener('click', (e) => {
+            i++;
+            if (i == arrayFavoriteIcon.length) {
+                i = 0;
+            }
+            e.target.src = arrayFavoriteIcon[i];
+        });
+    });
+}
+
+
+
+// window.addEventListener("resize", () => {
+//     if (window.innerWidth < 520) {
+//         getSlider(slidesDesk, 0);
+//         changeFavoriteIcon(favoriteIcon);
+
+//     }
+//     else {
+//         getSlider(slidesMob, 0);
+//         changeFavoriteIcon(favoriteIconMob);
+
+//     }
+// });
+
+getSlider(slidesDesk, 0);
+changeFavoriteIcon(favoriteIcon);
+
+getSlider(slidesMob, 0);
+changeFavoriteIcon(favoriteIconMob);
 
 
